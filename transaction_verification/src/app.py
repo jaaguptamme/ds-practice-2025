f1a2f760057123fd0bd267ee35c7fbe55308012a
Andres <andresalumets@hotmail.com>
2025-02-19 11:06:47 +0200

    changed names in app.py files

--- transaction_verification/src/app.py
+++ transaction_verification/src/app.py
@@ -5,21 +5,21 @@ import os
 # The path of the stubs is relative to the current file, or absolute inside the container.
 # Change these lines only if strictly needed.
 FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
-fraud_detection_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/fraud_detection'))
-sys.path.insert(0, fraud_detection_grpc_path)
-import fraud_detection_pb2 as fraud_detection
-import fraud_detection_pb2_grpc as fraud_detection_grpc
+transaction_verification_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/transaction_verification'))
+sys.path.insert(0, transaction_verification_grpc_path)
+import transaction_verification_pb2 as transaction_verification
+import transaction_verification_pb2_grpc as transaction_verification_grpc
 
 import grpc
 from concurrent import futures
 
 # Create a class to define the server functions, derived from
-# fraud_detection_pb2_grpc.HelloServiceServicer
-class HelloService(fraud_detection_grpc.HelloServiceServicer):
+# transaction_verification_pb2_grpc.HelloServiceServicer
+class HelloService(transaction_verification_grpc.HelloServiceServicer):
     # Create an RPC function to say hello
     def SayHello(self, request, context):
         # Create a HelloResponse object
-        response = fraud_detection.HelloResponse()
+        response = transaction_verification.HelloResponse()
         # Set the greeting field of the response object
         response.greeting = "Hello, " + request.name
         # Print the greeting message
@@ -31,7 +31,7 @@ def serve():
     # Create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor())
     # Add HelloService
-    fraud_detection_grpc.add_HelloServiceServicer_to_server(HelloService(), server)
+    transaction_verification_grpc.add_HelloServiceServicer_to_server(HelloService(), server)
     # Listen on port 50051
     port = "50051"
     server.add_insecure_port("[::]:" + port)
