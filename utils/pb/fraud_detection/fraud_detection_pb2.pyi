7b4ee3222c3d055ef6423dcfce4b31be1bd0e41c
Jaagup Tamme <jaaguptamme@gmail.com>
2025-02-19 11:08:51 +0200

    fraud proto

--- utils/pb/fraud_detection/fraud_detection_pb2.pyi
+++ utils/pb/fraud_detection/fraud_detection_pb2.pyi
@@ -1,8 +1,31 @@
+from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class Item(_message.Message):
+    __slots__ = ("name", "quantity")
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    QUANTITY_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    quantity: int
+    def __init__(self, name: _Optional[str] = ..., quantity: _Optional[int] = ...) -> None: ...
+
+class OrderRequest(_message.Message):
+    __slots__ = ("items",)
+    ITEMS_FIELD_NUMBER: _ClassVar[int]
+    items: _containers.RepeatedCompositeFieldContainer[Item]
+    def __init__(self, items: _Optional[_Iterable[_Union[Item, _Mapping]]] = ...) -> None: ...
+
+class OrderRepsonse(_message.Message):
+    __slots__ = ("is_fraud", "message")
+    IS_FRAUD_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    is_fraud: bool
+    message: str
+    def __init__(self, is_fraud: bool = ..., message: _Optional[str] = ...) -> None: ...
 
 class HelloRequest(_message.Message):
     __slots__ = ("name",)
