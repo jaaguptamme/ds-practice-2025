1404e418a56ad4905a32e7a4b94016b0dafc433f
Juhan Oskar Hennoste <juhan.hennoste@ut.ee>
2025-02-19 11:14:16 +0200

    Clean up froud

--- utils/pb/fraud_detection/fraud_detection_pb2.pyi
+++ utils/pb/fraud_detection/fraud_detection_pb2.pyi
@@ -26,15 +26,3 @@ class OrderRepsonse(_message.Message):
     is_fraud: bool
     message: str
     def __init__(self, is_fraud: bool = ..., message: _Optional[str] = ...) -> None: ...
-
-class HelloRequest(_message.Message):
-    __slots__ = ("name",)
-    NAME_FIELD_NUMBER: _ClassVar[int]
-    name: str
-    def __init__(self, name: _Optional[str] = ...) -> None: ...
-
-class HelloResponse(_message.Message):
-    __slots__ = ("greeting",)
-    GREETING_FIELD_NUMBER: _ClassVar[int]
-    greeting: str
-    def __init__(self, greeting: _Optional[str] = ...) -> None: ...
