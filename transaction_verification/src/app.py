8680f488b27f344794d33250eb68bae62d4fb1a3
Andres <andresalumets@hotmail.com>
2025-03-03 07:11:30 +0200

    Maybe completed order verification

--- transaction_verification/src/app.py
+++ transaction_verification/src/app.py
@@ -12,13 +12,34 @@ import transaction_verification_pb2_grpc as transaction_verification_grpc
 
 import grpc
 from concurrent import futures
+from datetime import datetime
+import re
 
 def verify_credit_card(request: transaction_verification.TransactionRequest):
     if len(str(request.cvv))!=3:
         return False
     if len(str(request.credit_card_number))!=16:
         return False
+    month=int(request.expiration_date.split('/')[0])
+    year=int(request.expiration_date.split('/')[1])
+    current_month = datetime.now().month
+    current_year = (datetime.now().year)%100
+    if current_year>year:
+        return False
+    if current_year==year and current_month>month:
+        return False
     return True
+
+def verify_billing_address(request: transaction_verification.TransactionRequest):
+    bad_countries  = ['Russia', 'North Korea', 'Spain', 'Greenland']
+    for country in bad_countries:
+        if country in request.billing_address:
+            return False
+    return True
+
+def verify_contact(request: transaction_verification.TransactionRequest):
+    valid  = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', request.contact)
+    return valid
 
 # Create a class to define the server functions, derived from
 # transaction_verification_pb2_grpc.HelloServiceServicer
@@ -29,13 +50,19 @@ class VerificationService(transaction_verification_grpc.VerificationServiceServi
         response = transaction_verification.TransactionResponse()
         # Set the greeting field of the response object
         is_correct=True
+        response.message = "Hello, you are get verification"
+
+        if verify_contact(request)==False:
+            response.message = "Given contact should be valid email"
+            is_correct=False
+
         if verify_credit_card(request)==False:
+            response.message = "Given credit card is not valid"
             is_correct=False
         
-        if is_correct:
-            response.message = "Hello, you are get verification"
-        else:
-            response.message = "Lick the pan"
+        if verify_billing_address(request)==False:
+            response.message = "We don't take orders from that country"
+            is_correct=False
 
         response.is_verified=is_correct
         # Print the greeting message
