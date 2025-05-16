8685ad4efb17e0fff6bb7035df64d2b00d3ceebd
Jaagup Tamme <jaaguptamme@gmail.com>
2025-05-16 16:45:17 +0300

    added basic API test

--- testing/cypress/e2e/tests.cy.js
+++ testing/cypress/e2e/tests.cy.js
@@ -1,6 +1,36 @@
-describe('Homepage', () => {
-  it('should load and show welcome message', () => {
-    cy.visit('http://localhost:8000'); // Change to your app's URL
-    cy.contains('Welcome'); // Adjust to something real from your UI
+describe('Page loads', () => {
+  it('should load and show checkout message', () => {
+    cy.visit('http://localhost:8080');
+    cy.contains('Checkout Page');
   });
 });
+
+describe('Valid signular order', () => {
+  it('should checkout and have the book counts updated', () => {
+    cy.visit('http://localhost:8080');
+    cy.contains('Submit Order').click();
+    cy.contains('Order status: Order Approved',  { timeout: 10000 }).should('be.visible');
+  });
+});
+
+describe('API test', () => {
+  it('handles single order via API', async () => {
+    let body = null;
+   await cy.fixture('basic_order.json').then((data)=>{
+            body=data;
+        })
+  const order1 =    { method: 'POST',
+        url: 'http://localhost:8081/checkout',
+        body: body
+    };
+  cy.wrap(Promise.all([
+    cy.request(order1),
+  ])).then((responses) => {
+    responses.forEach(response => {
+      expect(response.status).to.eq(200);
+      cy.log(response);
+    });
+  });
+}); 
+}
+)
