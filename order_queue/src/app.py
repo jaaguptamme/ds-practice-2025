4890b7bb08cc644687fb8066d584b68631078f12
Jaagup Tamme <jaaguptamme@gmail.com>
2025-05-26 18:31:04 +0300

    add gauge, counter, and up_down counter

--- order_queue/src/app.py
+++ order_queue/src/app.py
@@ -11,6 +11,7 @@ import common_pb2 as common
 from concurrent import futures
 import grpc
 import order_queue_pb2_grpc as order_queue_grpc
+from opentelemetry.metrics import Observation
 
 from tracing import get_tracer_and_meter
 
@@ -32,12 +33,32 @@ class RequestWithPriority:
 class OrderQueueService(order_queue_grpc.OrderQueueServiceServicer):
     def __init__(self):
         self._queue = queue.PriorityQueue()
+        meter.create_observable_gauge(
+            'orders_in_queue',
+            description='Current number of orders in the queue',
+            callbacks=[lambda _: [Observation(self._queue.qsize(), {})]]
+        )
+        # New metrics
+        self.failed_dequeues = meter.create_counter(
+            'failed_dequeues',
+            description='Number of failed dequeue attempts due to empty queue'
+        )
+        self.max_queue_size = meter.create_up_down_counter(
+            'max_queue_size',
+            description='Maximum observed queue size during runtime'
+        )
+        self._max_size = 0
     
     def Enqueue(self, request, context):
         self._queue.put(RequestWithPriority(request))
         orders_queued.add(1)
         orders_accepted.add(1)
         books_ordered.add(sum([item.quantity for item in request.items]))
+        # Track max queue size
+        current_size = self._queue.qsize()
+        if current_size > self._max_size:
+            self.max_queue_size.add(current_size - self._max_size)
+            self._max_size = current_size
         return common.Empty()
     
     def Dequeue(self, request, context):
@@ -46,6 +67,7 @@ class OrderQueueService(order_queue_grpc.OrderQueueServiceServicer):
             orders_queued.add(-1)
             return request
         except queue.Empty:
+            self.failed_dequeues.add(1)
             context.abort(grpc.StatusCode.ABORTED, 'Queue empty')
 
 def serve():
